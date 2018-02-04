d7c472e522c2cfdd139973c0836b1e1eaf7413dd
mlafaive <mlafaive@umich.edu>
2018-02-04 13:55:29 -0500

    main endpoint get users test

--- controllers/main.py
+++ controllers/main.py
@@ -7,16 +7,9 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def main_hello():
-    # db = connect_to_database()
-    # cur = db.cursor()
-
-    # cur.execute('SELECT id, name FROM Test')
-
-    # data = cur.fetchall()
-    # print(data)
-    # resp = jsonify(data)
-    # resp.status_code = 200
-    # return resp
-    return 'hello world'
+    all_users = User.query.all()
+    resp = jsonify(data)
+    resp.status_code = 200
+    return resp
 
 
