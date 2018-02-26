7c185089952a14e6391e1ba04dbf55ef1abc8300
Anthony Opipari <topipari@umich.edu>
2018-02-25 19:17:00 -0500

    Adding offers table implementation.

---
+++ models/offers.py
@@ -0,0 +1,34 @@
+from extensions import db
+
+class Offers(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    business_id = db.Column(db.Integer, nullable=False)
+    start_time = db.Column(db.Date, nullable=False)
+    end_time = db.Column(db.Date, nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, title, description, business_id, start_time, end_time):
+        self.title = title
+        self.description = description
+        self.business_id = business_id
+        self.start_time = start_time
+        self.end_time = end_time
+
+
+    def __repr__(self):
+        return "<business_id=%d, start_time=%r, end_time=%r, title='%s', description='%s'>" % \
+        		(self.business_id, self.start_time, self.end_time, self.title, self.description)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'id': self.id,
+           'business_id': self.business_id,
+           'start_time': self.start_time,
+           'end_time': self.end_time,
+           'title': self.title,
+           'description': self.description,
+       }
+
