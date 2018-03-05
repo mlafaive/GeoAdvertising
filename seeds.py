ab7dd523dfa66f22cf1be57ae1237cd8036b0bc6
mlafaive <mlafaive@umich.edu>
2018-03-05 16:40:39 -0500

    fixed seeds bug

--- seeds.py
+++ seeds.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 			db.create_all()
 			db.session.commit()
 
-			User.query.delete()
+			Users.query.delete()
 			print('To reset ids run reset from heroku account')
 			
 		
