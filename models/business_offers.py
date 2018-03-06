7a53cad35bae465ae3f925e14aae3b43c0667132
Anthony Opipari <topipari@umich.edu>
2018-03-06 16:06:33 -0500

    Adding businessss_offers table.

---
+++ models/business_offers.py
@@ -0,0 +1,22 @@
+from extensions import db
+
+class Business_Offers(db.Model):
+    business_id = db.Column(db.Integer, primary_key=True)
+    offer_id = db.Column(db.Integer, primary_key=True)
+
+    def __init__(self, business_id, offer_id):
+        self.business_id = business_id
+        self.offer_id = offer_id
+
+
+    def __repr__(self):
+        return "<business_id=%d, offer_id=%d>" % \
+              (self.business_id, self.offer_id)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'business_id': self.business_id,
+           'offer_id': self.offer_id,
+       }
