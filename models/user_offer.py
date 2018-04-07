14fcbfc1d06004caa0aba48adb60e4bac2fa7914
Anthony Opipari <topipari@umich.edu>
2018-04-07 19:25:53 -0400

    Adding view tracking to SingleOffer get API endpoint. Removing old user_offer relationship.

--- models/user_offer.py
+++
@@ -1,7 +0,0 @@
-from extensions import db
-
-
-user_offer = db.Table('user_offer',
-                          db.Column('user_address', db.String(50), db.ForeignKey('user.email'), primary_key=True),
-                          db.Column('offer_id', db.Integer, db.ForeignKey('offer.id'), primary_key=True)
-                        )
