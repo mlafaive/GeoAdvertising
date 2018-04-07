ca8b1110361049aaf9e60270e12a3f736283cd65
Anthony Opipari <topipari@umich.edu>
2018-04-07 16:34:17 -0400

    Remove resource from app.py for userOffers endpoint.

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ api.add_resource(BusinessOffers, '/api/businesses/<int:_id>/offers')
 api.add_resource(AllOffers, '/api/offers')
 api.add_resource(SingleOffer, '/api/offers/<int:_id>')
 api.add_resource(UserBusinesses, '/api/users/<string:_email>/businesses')
-api.add_resource(UserOffers, '/api/users/<string:_email>/offers')
 api.add_resource(AllInterests, '/api/interests')
 
 # Register the controllers
